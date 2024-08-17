3f73d65b05226e28f0f1d8735e531dc84b443818
Henock Dinberu <henockdinberu11@gmail.com>
2024-08-17 01:54:06 -0400

    fixed gun

--- gun/pistol/gun.gd
+++ gun/pistol/gun.gd
@@ -4,15 +4,30 @@ extends Area2D
 @onready var weapon_pivot = $WeaponPivot
 @onready var gun = $"."
 
+var enemies = []
+
 func _physics_process(_delta):
 	var enemies_in_range = gun.get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		gun.look_at(target_enemy.global_position)
 		if target_enemy.global_position < weapon_pivot.global_position:
-			pistol.flip_v = true
+			pistol.flip_v = true;
 		else:
-			pistol.flip_v = false
+			pistol.flip_v = false;
 		
 		# print("enemies_in_range: ", enemies_in_range)
-	
+
+func enter():
+	enemies = get_tree().get_nodes_in_group("Enemy");
+
+func shoot():
+	const BULLET = preload("res://projectile/bullet.tscn")
+	var new_bullet = BULLET.instantiate()
+	new_bullet.global_position = %ShootingPoint.global_position
+	new_bullet.global_rotation = %ShootingPoint.global_rotation
+	%ShootingPoint.add_child(new_bullet)
+
+# Begins shooting gun after timer cooldown ends
+func _on_timer_timeout():
+	shoot()
