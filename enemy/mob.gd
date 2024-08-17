c27b1f955d6928385f5413e0f5e02481c98dd1ae
Aidan Sommer <aidansommeriii@gmail.com>
2024-08-16 23:43:26 -0400

    Groups
    Added the enemy and player to groups for easier look up for the auto attack.

--- enemy/mob.gd
+++ enemy/mob.gd
@@ -16,3 +16,5 @@ func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * speed
 	move_and_slide()
+
+
--- gun/gun.gd
+++ gun/gun.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+@onready var collision_shape_2d = $CollisionShape2D
+
 func _physics_process(_delta):
 	var enemies_in_range = get_overlapping_bodies()
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
-		print("enemies_in_range: ", enemies_in_range)
+		# print("enemies_in_range: ", enemies_in_range)
