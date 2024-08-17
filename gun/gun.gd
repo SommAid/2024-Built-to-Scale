81a2947ac640f88920ebcb499dd5ef7c2f7fd2b5
Aidan Sommer <aidansommeriii@gmail.com>
2024-08-17 00:19:39 -0400

    Named layers

--- gun/gun.gd
+++ gun/gun.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 @onready var collision_shape_2d = $CollisionShape2D
+@onready var pistol = $WeaponPivot/Pistol
 
 var enemies = []
 
@@ -9,6 +10,7 @@ func _physics_process(_delta):
 	if enemies_in_range.size() > 0:
 		var target_enemy = enemies_in_range.front()
 		look_at(target_enemy.global_position)
+		
 		# print("enemies_in_range: ", enemies_in_range)
 
 func enter():
