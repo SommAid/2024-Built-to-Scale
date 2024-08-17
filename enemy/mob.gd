3a3ab68a344a7ff76df57a53aa2d00d45090cb02
Aidan Sommer <aidansommeriii@gmail.com>
2024-08-16 23:04:08 -0400

    Cleaned up slime and unused params
    Cleaned up the slime/mob V1 by using animatedsprite2D.

--- enemy/mob.gd
+++ enemy/mob.gd
@@ -1,15 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@export var speed : int = 50
+@export var jump_velocity : int = -400
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
 @onready var player = get_node("/root/TestLevel/PlayerCat")
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 #var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	animated_sprite_2d.play("walk")
 
-
-func _physics_process(delta):
+func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * 300
+	velocity = direction * speed
 	move_and_slide()
