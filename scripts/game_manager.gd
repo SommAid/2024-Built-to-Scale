1bcb7d73fc3ab38362f6731a3d9edc714c19328a
Aidan Sommer <aidansommeriii@gmail.com>
2024-08-17 18:30:41 -0400

    Better pausing
    Fixed some merge issues and main/pause menus are better at cleaning up.

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -15,13 +15,15 @@ func pause_game():
 	
 
 func resume_game():
+	if PAUSE_MENU_SCREEN_instance != null && pause_state:
+		PAUSE_MENU_SCREEN_instance.queue_free()
 	if pause_state:
 		pause_state = false
-		PAUSE_MENU_SCREEN_instance.queue_free()
 		get_tree().paused = pause_state
 
 func menu_menu():
 	var main_menu_screen_instance = main_menu_screen.instantiate()
+	get_tree().current_scene.queue_free()
 	get_tree().get_root().add_child(main_menu_screen_instance)
 	
 func exit_game():
