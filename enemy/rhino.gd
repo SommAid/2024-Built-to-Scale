1e77c11e8ed892fb580db39b300a2843371c2b31
Rukudzo Mushunje <102190023+Ruklee13@users.noreply.github.com>
2024-08-18 19:41:37 -0400

    fixed spawner locations

---
+++ enemy/rhino.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+@export_category("Enemy Velocity")
+@export var speed : int = 50
+@export var jump_velocity : int = -400
+
+@export_category("Enemy Health")
+@export var health_amount : int = 5
+
+@export_category("Enemy Damage")
+@export var damage_amount : int = 1
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+# @onready var player = get_node("/root/TestLevel/PlayerCat")
+var player : CharacterBody2D
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	animated_sprite_2d.play("run")
+	var player_list = get_tree().get_nodes_in_group("Player")
+	if player_list.size() > 0:
+		player = player_list[0] as CharacterBody2D
+
+func _physics_process(_delta):
+	var direction : Vector2
+	if player != null:
+		direction = global_position.direction_to(player.global_position)
+	else:
+		speed = 0
+	velocity = direction * speed
+	move_and_slide()
+	
+
+func deal_damage() -> int:
+	return damage_amount
+
+
+func _on_hurtbox_body_entered(body):
+	if body.has_method("get_damage_amount"):
+		var node = body as Node
+		health_amount -= node.damage_amount
+		# print("Health amount: ", str(health_amount))
+		if health_amount <= 0:
+			queue_free()
--- enemySpawner.gd
+++ enemySpawner.gd
@@ -6,11 +6,19 @@ var bee = preload("res://enemy/bee.tscn")
 var greenSlime = preload("res://enemy/mob.tscn")
 var spawnPoints = []
 var enemyList = [slime,bee,greenSlime]
+var player: CharacterBody2D
+var direction: Vector2
+var distance = 300
+var new_posiotn: Vector2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	for i in get_children():
 		if i is Marker2D:
 			spawnPoints.append(i)
+	var player_list = get_tree().get_nodes_in_group("Player")
+	if player_list.size() > 0:
+		player = player_list[0] as CharacterBody2D
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -19,7 +27,9 @@ func _process(delta):
 
 
 func _on_timer_timeout():
-	var spawn = spawnPoints[randi() % spawnPoints.size()]
-	var enemy = enemyList[randi() % enemyList.size()].instantiate()
-	enemy.position = spawn.position
-	main.add_child(enemy)
+	if player != null:
+		direction = (position-player.position).normalized()
+		var spawn = spawnPoints[randi() % spawnPoints.size()]
+		var enemy = enemyList[randi() % enemyList.size()].instantiate()
+		enemy.position = player.position+(direction*distance)
+		main.add_child(enemy)
