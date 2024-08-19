2f80e5f918e20e743e51128bdf3357b6c97bb6ce
Aidan Sommer <aidansommeriii@gmail.com>
2024-08-19 02:17:41 -0400

    Added a blue amoeba like enemy
    Be careful this enemy will charge at you when provoked and can deal large amounts of damage.

---
+++ enemy/Completed Mobs/blue amoeba/blue_amoeba.gd
@@ -0,0 +1,107 @@
+extends CharacterBody2D
+
+@export_category("Enemy Velocity")
+@export var enemy_speed : int = 28
+@export var rage_speed : int = 125
+
+@export_category("Enemy Health")
+@export var health_amount : int = 25
+
+@export_category("Enemy Attack")
+@export var damage_amount : int = 2
+
+var slime_glob = preload("res://enemy/Completed Mobs/Blueraspberry Slime/slime_glob.tscn")
+
+@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var weapon_area = $WeaponArea
+@onready var rage_duration = $RageDuration
+@onready var rage_cooldown = $RageCooldown
+
+
+var player : CharacterBody2D
+enum enemy_state {Attack, Walk, Rage, Charge, Break}
+var current_state : enemy_state = enemy_state.Walk
+
+var is_dying : bool = false
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+func _ready():
+	# weapon_area.set_collision_mask_value()
+	animated_sprite_2d.play("walk")
+	var player_list = get_tree().get_nodes_in_group("Player")
+	if player_list.size() > 0:
+		player = get_tree().get_nodes_in_group("Player")[0] as CharacterBody2D
+
+func _physics_process(_delta):
+	var direction : Vector2 = Vector2.ZERO
+	var speed = 0
+	if current_state == enemy_state.Rage:
+		speed = rage_speed
+	elif current_state == enemy_state.Walk:
+		speed = enemy_speed
+	if player != null:
+		direction = global_position.direction_to(player.global_position)
+		var flip = false if direction[0] > 0 else true
+		animated_sprite_2d.flip_h = flip
+	
+	move(direction, speed)
+	animation_handler()
+	move_and_slide()
+	
+func move(direction : Vector2, speed : int):
+	velocity = direction * speed
+	
+func _on_weapon_area_area_entered(area):
+	if area.is_in_group("Player") and current_state == enemy_state.Rage:
+		weapon_area.set_collision_layer_value(2, true)
+		current_state = enemy_state.Attack
+		animated_sprite_2d.animation_finished
+
+func deal_damage() -> int:
+	return damage_amount
+
+func _on_hurtbox_area_entered(area):
+	print("Rager got hit")
+	if area.has_method("get_damage_amount"):
+		var node = area as Node
+		health_amount -= node.damage_amount
+		# print("Health amount: ", str(health_amount))
+		if health_amount <= 0:
+			queue_free()
+			# current_state = enemy_state.Dying
+			# animated_sprite_2d.play("death")
+			animated_sprite_2d.animation_finished
+
+func animation_handler():
+	if current_state == enemy_state.Walk:
+		animated_sprite_2d.play("walk")
+	elif current_state == enemy_state.Attack:
+		animated_sprite_2d.play("attack")
+	elif current_state == enemy_state.Rage:
+		animated_sprite_2d.play("rage")
+
+func _on_animated_sprite_2d_animation_finished():
+	if current_state == enemy_state.Attack:
+		weapon_area.set_collision_layer_value(2, false)
+		current_state = enemy_state.Rage
+	elif current_state == enemy_state.Charge:
+		current_state = enemy_state.Rage
+		rage_duration.start()
+	elif current_state == enemy_state.Break:
+		current_state = enemy_state.Walk
+	else:
+		queue_free()
+
+func _on_rage_duration_timeout():
+	current_state = enemy_state.Break
+	animated_sprite_2d.play_backwards("charge")
+	animated_sprite_2d.animation_finished
+	rage_cooldown.start()
+
+func _on_rage_cooldown_timeout():
+	current_state = enemy_state.Charge
+	animated_sprite_2d.play("charge")
+	animated_sprite_2d.animation_finished
+	
+	
