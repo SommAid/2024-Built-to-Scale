d4dc70b594a3d634fcd7bb982d2707b8e4567f6c
Rukudzo Mushunje <102190023+Ruklee13@users.noreply.github.com>
2024-08-18 22:47:30 -0400

    my stuff

--- enemy/soldier.gd
+++ enemy/soldier.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 @export_category("Enemy Velocity")
-@export var speed : int = 50
+@export var speed : int = 100
 @export var jump_velocity : int = -400
 
 @export_category("Enemy Health")
-@export var health_amount : int = 5
+@export var health_amount : int = 1000
 
 @export_category("Enemy Damage")
 @export var damage_amount : int = 1
@@ -13,7 +13,7 @@ extends CharacterBody2D
 @onready var animated_sprite_2d = $AnimatedSprite2D
 # @onready var player = get_node("/root/TestLevel/PlayerCat")
 var player : CharacterBody2D
-enum animationList {hit, idle, run}
+enum animationList {hit, idle, walk, attack, die}
 var state = animationList.idle
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -21,7 +21,7 @@ var state = animationList.idle
 func _ready():
 	if state == animationList.idle:
 		animated_sprite_2d.play("idle")
-		state = animationList.run
+		state = animationList.walk
 	var player_list = get_tree().get_nodes_in_group("Player")
 	if player_list.size() > 0:
 		player = player_list[0] as CharacterBody2D
@@ -29,15 +29,20 @@ func _ready():
 func _physics_process(_delta):
 	var direction : Vector2
 	if player != null:
-		if state == animationList.run:
-			animated_sprite_2d.play("run")
+		if state == animationList.walk:
+			animated_sprite_2d.play("walk")
 		elif state == animationList.idle:
 			animated_sprite_2d.play("idle")
-		else:
+		elif state == animationList.hit:
 			animated_sprite_2d.play("hit")
-			state = animationList.run
+			state = animationList.walk
+		elif state == animationList.die:
+			animated_sprite_2d.play("die")
+		else:
+			animated_sprite_2d.play("attack")
+			state = animationList.walk
 		direction = global_position.direction_to(player.global_position)
-		var flip = false if direction[0] < 0 else true
+		var flip = true if direction[0] < 0 else false
 		animated_sprite_2d.flip_h = flip
 			
 	else:
@@ -54,6 +59,8 @@ func _on_hurtbox_area_entered(area):
 		state = animationList.hit
 		var node = area as Node
 		health_amount -= node.damage_amount
-		print("something: ", str(health_amount))
+		print("anything: ", str(health_amount))
 		if health_amount <= 0:
+			state = animationList.die
 			queue_free()
+
