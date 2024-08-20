2d8f7771edd33b38a7218ff0e3da778517ebc42d
Rukudzo Mushunje <102190023+Ruklee13@users.noreply.github.com>
2024-08-20 08:38:50 -0400

    added proper spawners to level

--- Cheese.gd
+++ Cheese.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var player : CharacterBody2D
-var healthBoost = 10
+var healthBoost = 1
 func _ready():
 	# weapon_area.set_collision_mask_value()
 	var player_list = get_tree().get_nodes_in_group("Player")
@@ -13,6 +13,8 @@ func _ready():
 func _on_hurtbox_area_entered(area):
 	if area.is_in_group("Player"):
 		player.health += healthBoost
+		if player.health > player.max_player_health:
+			player.health = player.max_player_health
 		player.update_health_ui()
 		#print(player.health)
 		queue_free()
